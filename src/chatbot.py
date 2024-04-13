f8996f5bdfd250253377012e93b97d34fe22bbfb
Pradeep Arya <impradeeparya@yahoo.com>
2024-04-13 15:01:28 +0530

    resolve user query for state and districts

--- src/chatbot.py
+++ src/chatbot.py
@@ -1,3 +1,5 @@
+from nltk.tokenize import word_tokenize
+
 from src.state_info import StateInfo
 
 
@@ -5,9 +7,29 @@ class ChatBot:
     def __init__(self):
         self.state_info = StateInfo()
 
-    def respond(self, user_input):
-        state_info = {}
-        for state in self.state_info.data:
-            if state['state'] == user_input:
-                state_info = state
-        return state_info
+    def respond(self, query):
+
+        state_name, district_name = self.process_query(query)
+        if state_name and district_name:
+            return f"{district_name} is a district in {state_name}."
+        elif state_name:
+            return f"{state_name} is a state."
+        else:
+            return "Pardon"
+
+    def process_query(self, query):
+
+        # Tokenize the query
+        tokens = word_tokenize(query)
+
+        # Example logic to identify state and district names
+        state_name = None
+        district_name = None
+        for token in tokens:
+            if token.lower() in self.state_info.data:
+                state_name = token.lower()
+            elif state_name and token.lower() in self.state_info.data[state_name]:
+                district_name = token.lower()
+                break
+
+        return state_name, district_name
--- src/state_info.py
+++ src/state_info.py
@@ -9,6 +9,12 @@ class StateInfo:
         print("Absolute path:", os.path.abspath(json_file_path))
 
         with open(json_file_path, 'r') as states_and_districts:
-            states = json.load(states_and_districts)
-            self.data = states['states']
+            self.states = json.load(states_and_districts)
+            self.data = self.populate_states_data()
         print(len(self.data), " states data loaded")
+
+    def populate_states_data(self):
+        data = {}
+        for state in self.states['states']:
+            data[state['state'].lower()] = [s.lower() for s in state['districts']]
+        return data
